e4de7871dac2236806dc68e141a5031fb041a150
SmileZD <38121455+SmileZD@users.noreply.github.com>
2022-01-17 14:12:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ app.get('/', function (req, res) {
 })
 var server = net.createServer(function(client) {
         client.on('error',function(err){
-        	client.end();
-        	client.destroy();
-        	try{
+        	try{	
 		ser.end();
         	ser.destroy();
+        	client.end();
+        	client.destroy();
         	}catch(err){}
         })
 	var ser = net.connect({port: serverport,host: serverip},function() {
@@ -76,9 +76,12 @@ var server = net.createServer(function(client) {
 	client.on('data',function(data) {
             	data.toString().split('912104410').forEach(jsonDataStr => {
             		if (trim(jsonDataStr).length) {
-            			let buff=key.decryptPublic(trim(jsonDataStr), 'utf8');
-            			buff=buff.slice(0,buff.length-1)
-            			try{ser.write(Buffer.from(buff))}catch(err){}}})
+            			let buff;
+            			try{
+            				buff=key.decryptPublic(trim(jsonDataStr), 'utf8');
+            				buff=buff.slice(0,buff.length-1)
+            				try{ser.write(Buffer.from(buff))}catch(err){}
+            			}catch(err){}
         })
 })
 server.on('error', function (err) {setTimeout(function(){serverfun(port2+1)},1000)});
